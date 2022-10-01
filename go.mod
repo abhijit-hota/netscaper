82d83133ac5ebef7c020fd0c715c4ba5a63995fd
abhijit-hota <abhihota025@gmail.com>
2022-10-01 20:36:24 +0530

    cleanup stuff

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/abhijit-hota/netscaper
 
 go 1.18
-
-require (
-	github.com/kr/pretty v0.3.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/rogpeppe/go-internal v1.6.1 // indirect
-)
--- netscaper_test.go
+++ netscaper_test.go
@@ -1,15 +1,12 @@
 package netscaper
 
 import (
-	"q"
 	"testing"
 )
 
 func TestBasicParser(t *testing.T) {
-	bookmarks, err := Parse("<!DOCTYPE NETSCAPE-Bookmark-file-1>ffsdffsdfsdf", nil)
+	_, err := Parse("<!DOCTYPE NETSCAPE-Bookmark-file-1>ffsdffsdfsdf", nil)
 	if err != nil {
 		panic(err)
 	}
-	// testing.
-	q.Q(bookmarks) // DEBUG
 }
